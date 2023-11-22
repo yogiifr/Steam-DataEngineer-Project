b0bbf9c5b994b2eae7dab374869d7914d0050d5e
yogiifr <yogiifr@gmail.com>
2023-11-22 15:42:29 +0700

    Created GCS & BigQuery

---
+++ terraform/variables.tf
@@ -0,0 +1,44 @@
+locals {
+  data_lake_bucket = "steamgames_project"
+}
+
+variable "project" {
+  description = "Project number/id"
+  default = "handy-honor-401601"
+}
+
+variable "region" {
+  description = "Region for GCP resources. Choose as per your location: https://cloud.google.com/about/locations"
+  default = "us-east1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "steam_data"
+}
+
+variable "BQ_DATASET_EXTERNAL" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "steam_data_external"
+}
+
+variable "BQ_DATASET_ANALYTICS" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "steam_data_analytics"
+}
+
+variable "credentials" {
+  description = "Absolute path to service account's key"
+  type = string
+  default = "../service_account.json"
+  # D:\1--Data_Zoomcamp\final_project\steamgames_project\service_account.json
+}
